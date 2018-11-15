5438486a5f398f35f8866f1e71e57a3dabda3dde
renxs <12345678>
2018-11-15 15:52:21 +0800

    mm

--- system_modules/database/dbconfig.js
+++ system_modules/database/dbconfig.js
@@ -1,5 +1,8 @@
 module.exports={
     'host':'127.0.0.1',
-    'dbname':'hx'
+    'port':27017,
+    'dbname':'hx',
+    'username':'hx',
+    'pwd':'12345678'
 };
 
--- system_modules/database/mongodb.js
+++ system_modules/database/mongodb.js
@@ -1,17 +1,20 @@
-var config = require('./dbconfig');
-var mongoose = require('mongoose');
-var autoinc    = require('mongoose-id-autoinc');
+const config = require('./dbconfig');
+const mongoose = require('mongoose');
+const autoinc    = require('mongoose-id-autoinc');
 
-module.exports = mongoose;
 
-mongoose.connect(config.host, config.dbname);
+const  uri = 'mongodb://'+config.username+':'+config.pwd+'@'+config.host+':'+config.port+'/'+config.dbname;
+mongoose.connect(uri);
 
 //数据库连接
-var db = mongoose.connection;
+const db = mongoose.connection;
 autoinc.init(db);
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback() {
     console.log('已经连接到数据库');
 });
+
+
+module.exports = mongoose;
 
 
